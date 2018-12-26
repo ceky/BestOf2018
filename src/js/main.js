63bf1104ac81864169252e3abf7683ea2a348b01
ceky <cekyy07@gmail.com>
2018-12-26 23:50:56 +0200

    Add animations on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
 setTimeout(() => {
-    ScrollReveal().reveal('.header-text', { duration: 800, scale: 0.8, interval: 150, distance: '2em' });
-    ScrollReveal().reveal('.carousel', { duration: 800, scale: 0.8 });
-    ScrollReveal().reveal('.ribbon', { duration: 1000, scale: 0.8, interval: 250, distance: '7em' });
     ScrollReveal().reveal('.item', { duration: 1000, scale: 0.8, interval: 250, distance: '7em' });
 }, 100);
